fd72c7f14d184bdb2dbec19c78845d5dd690ac19
Konstantin Troitskiy <atr.konst@gmail.com>
2022-03-10 12:49:14 +0100

    settings for mongo connection

--- src/modules/app.module.ts
+++ src/modules/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from '../controllers/app.controller';
 import { AppService } from '../services/app.service';
 
+const { MONGO_USERNAME, MONGO_PASSWORD, MONGO_HOST, MONGO_PORT, MONGO_DB } =
+  process.env;
+
+const mongoConnectionString = `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@estate-server_mongo_1:${MONGO_PORT}/${MONGO_DB}-new?authSource=admin&readPreference=primary&directConnection=true&ssl=false`;
+
 @Module({
-  imports: [],
+  imports: [MongooseModule.forRoot(mongoConnectionString)],
   controllers: [AppController],
   providers: [AppService],
 })
