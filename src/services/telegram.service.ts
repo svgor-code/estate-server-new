cc29cc255d9ff8880db1883df16fa7559d209987
Konstantin Troitskiy <atr.konst@gmail.com>
2022-03-25 19:19:56 +0100

    fix tg message

--- src/services/telegram.service.ts
+++ src/services/telegram.service.ts
@@ -54,10 +54,6 @@ export class TelegramService {
   private getApartmentMessageTemplete(apartment: IApartment): string {
     const { title, address, price, pricePerMeter, href } = apartment;
 
-    return `
-      ${title} \n 
-      Адрес: ${address} \n
-      Цена: ${price} рублей (${pricePerMeter} руб. за кв.м.) \n 
-      Ссылка: ${href}`;
+    return `${title} \n Адрес: ${address} \n Цена: ${price} рублей (${pricePerMeter} руб. за кв.м.) \nСсылка: ${href}`;
   }
 }
