8ac0682e651515441da354cf288ce35ef716284e
Konstantin Troitskiy <atr.konst@gmail.com>
2022-03-26 01:46:57 +0100

    fix cors

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './modules/app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: ['http://localhost:3000', 'http://84.201.141.110:3000/'],
   });
   await app.listen(8080);
 }
